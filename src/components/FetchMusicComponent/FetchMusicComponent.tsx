187554e802d216283e1e056c5a2a2413c8d6a4fe
KfWelch <kylef.welch@gmail.com>
2022-01-22 21:33:26 -0600

    finished recursion

--- src/components/FetchMusicComponent/FetchMusicComponent.tsx
+++ src/components/FetchMusicComponent/FetchMusicComponent.tsx
@@ -1,25 +1,33 @@
 
 import _ from 'lodash';
 import React, { useEffect, useState } from 'react';
-import { Button, FlatList, Text, View } from 'react-native';
+import { Button, FlatList, SafeAreaView, Text, View } from 'react-native';
 import { check, PERMISSIONS, request, RESULTS } from 'react-native-permissions';
 import Toast from 'react-native-toast-message';
 import * as RNFS from 'react-native-fs';
 import styles from './FetchMusicComponent.style';
 import { Album, Artist, Song } from '../../models/MusicModel';
 import { useTypedSelector } from '../../state/reducers';
+import { useDispatch } from 'react-redux';
+import { addArtist } from '../../state/actions/Albums';
 
 const FetchMusicComponent = () => {
     const [currentFiles, setCurrentFiles] = useState<RNFS.ReadDirItem[]>([]);
     const [filesList, setFilesList] = useState<RNFS.ReadDirItem[]>([]);
     const [foundMusicFiles, setFoundMusicFiles] = useState<Album[]>([]);
     const artists = useTypedSelector(state => state.Albums);
+    const dispatch = useDispatch();
 
     const getPermission = () => {
         check(PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE)
             .then(result => {
                 switch (result) {
-                    case RESULTS.GRANTED: break;
+                    case RESULTS.GRANTED: 
+                        Toast.show({
+                            type: 'info',
+                            text1: 'Already have permissions'
+                        });
+                        break;
                     case RESULTS.UNAVAILABLE:
                         Toast.show({ type: 'error', text1: 'Unable to access external storage' });
                         break;
@@ -41,58 +49,53 @@ const FetchMusicComponent = () => {
             })
     };
 
-    const getMusicFiles = async (recursiveFiles: RNFS.ReadDirItem[], artist?: Artist): Artist => {
-        const directories = recursiveFiles.filter(item => item.isDirectory());
-        const albums: Album[] = [];
+    const getMusicFiles = async (recursiveFiles: RNFS.ReadDirItem[], artist?: Artist): Promise<Artist[]> => {
+        const directories = recursiveFiles.filter(item => item.isDirectory() && !item.name.startsWith('.'));
+        const tempArtist: Artist = { ...(artist || { artist: '', albums: [] }) };
+        let artists: Artist[] = [];
 
         // 1st iteration, for each artist
         // 2nd iteration, for each album
-        directories.forEach(directory => {
-            RNFS.readDir(directory.path)
-                .then(files => {
-                    // 1st iteration, we are looking at an artist, and recurse to go through all albums
-                    if (!artist) {
-                        getMusicFiles(files, { artist: directory.name, albums: [] });
-                    // 2nd iteration, we are looking at an album, and we already have all the songs in the files
-                    } else {
-                        const songs: Song[] = [];
-                        files.forEach((songFile, index) => {
-                            const song: Song = {
-                                title: songFile.name,
-                                albumName: directory.name,
-                                numberInAlbum: index,
-                                path: songFile.path
-                            };
-                            songs.push(song);
-                        });
-                        const album: Album = {
-                            albumName: directory.name,
-                            songs
-                        };
-                        const tempArtist: Artist = {
-                            artist: artist.artist,
-                            albums: artist.albums.concat([album])
-                        };
-                    }
-                })
-        })
+        // By iteration, I mean levels deep of the recursion
+        for (let i = 0; i < directories.length; i++) {
+            const files = await RNFS.readDir(directories[i].path);
+            // 1st iteration, we are looking at an artist, and recurse to go through all albums
+            if (!artist) {
+                artists = artists.concat(await getMusicFiles(files, { artist: directories[i].name, albums: [] }));
+            // 2nd iteration, we are looking at an album, and we already have all the songs in the files
+            } else {
+                const songs: Song[] = [];
+                files.forEach((songFile, index) => {
+                    const song: Song = {
+                        title: songFile.name,
+                        albumName: directories[i].name,
+                        numberInAlbum: index,
+                        path: songFile.path
+                    };
+                    songs.push(song);
+                });
+                const album: Album = {
+                    albumName: directories[i].name,
+                    songs
+                };
+                tempArtist.albums = tempArtist.albums.concat([album])
+            }
+        }
+        if (artist) {
+            return Promise.resolve([tempArtist]);
+        } else {
+            return Promise.resolve(artists);
+        }
     }
 
     const getFiles = async () => {
         const path = `${RNFS.ExternalStorageDirectoryPath}/Music`
-        RNFS.readDir(path)
-            .then(files => {
-                setCurrentFiles(files);
-                setFilesList(files.filter(file => file.isFile()));
-                console.log(files);
-            })
-            .catch(e => {
-                Toast.show({
-                    type: 'error',
-                    text1: e.message,
-                    text2: e.code
-                });
-            });
+        const files = await RNFS.readDir(path)
+        const musicFiles = await getMusicFiles(files);
+        musicFiles.forEach(artist => dispatch(addArtist(artist)));
+        setCurrentFiles(files);
+        setFilesList(files.filter(file => file.isFile()));
+        console.log(files);
     };
 
     const itemSeparator = () => (<View style={styles.itemSeparator} />);
@@ -106,7 +109,7 @@ const FetchMusicComponent = () => {
     );
 
     return (
-        <View style={styles.container}>
+        <SafeAreaView style={styles.container}>
             <Button onPress={getPermission} title="get folder permission" />
             {itemSeparator()}
             <Button onPress={getFiles} title="scan for music" />
@@ -116,7 +119,7 @@ const FetchMusicComponent = () => {
                 keyExtractor={item => item.name}
                 ItemSeparatorComponent={itemSeparator}
             />
-        </View>
+        </SafeAreaView>
     )
 };
 
